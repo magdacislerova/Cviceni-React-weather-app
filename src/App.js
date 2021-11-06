874d61345d636a726c89148b432617c270fc939c
magdacislerova <magda.cislerova@gmail.com>
2021-11-06 23:17:23 +0100

    extract fetch functions out of useEffect again

--- src/App.js
+++ src/App.js
@@ -8,27 +8,29 @@ import Forecast from './components/Forecast/Forecast';
 import Spinner from './components/Spinner/Spinner';
 
 const App = () => {
+  const API_ID = process.env.REACT_APP_MY_API_ID;
+
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('Prague');
   const [forecasts, setForecasts] = useState(null);
 
-  useEffect(() => {
-    const API_ID = process.env.REACT_APP_MY_API_ID;
-    const fetchWeather = (city) => {
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_ID}`,
-      )
-        .then((response) => response.json())
-        .then((json) => setWeather(json));
-    };
+  const fetchWeather = (city) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_ID}`,
+    )
+      .then((response) => response.json())
+      .then((json) => setWeather(json));
+  };
 
-    const fetchForecasts = (city) => {
-      fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_ID}`,
-      )
-        .then((response) => response.json())
-        .then((json) => setForecasts(getFilteredForecasts(json.list)));
-    };
+  const fetchForecasts = (city) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_ID}`,
+    )
+      .then((response) => response.json())
+      .then((json) => setForecasts(getFilteredForecasts(json.list)));
+  };
+
+  useEffect(() => {
     fetchWeather(city);
     fetchForecasts(city);
   }, [city]);
