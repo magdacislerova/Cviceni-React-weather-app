8a4049fdb5802243f50fe8302a76d8f00df0c7e5
magdacislerova <magda.cislerova@gmail.com>
2021-11-07 10:47:37 +0100

    delete unnecessary button styles and clean up

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import './style.css';
 import { getDate } from '../../utils/utils';
 
-const Forecast = ({ forecast }) => {
-  return (
-    <div className="forecast">
-      <div className="forecast__day">{getDate(forecast.dt)}</div>
-      <div className="forecast__icon">
-        <img
-          src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
-          style={{ height: '100%' }}
-          alt="current weather icon"
-        />
-      </div>
-      <div className="forecast__temp"> {Math.round(forecast.main.temp)} °C</div>
+const Forecast = ({ forecast }) => (
+  <div className="forecast">
+    <div className="forecast__day">{getDate(forecast.dt)}</div>
+    <div className="forecast__icon">
+      <img
+        src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
+        style={{ height: '100%' }}
+        alt="current weather icon"
+      />
     </div>
-  );
-};
+    <div className="forecast__temp"> {Math.round(forecast.main.temp)} °C</div>
+  </div>
+);
 
 export default Forecast;
